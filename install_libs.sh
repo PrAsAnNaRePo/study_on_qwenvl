29d9d7dbaee536aba2c21a55fa7aead29f47edc3
PrAsAnNaRePo <prasannatwenty@gmail.com>
2025-05-04 22:33:04 +0530

    push to hub

--- install_libs.sh
+++ install_libs.sh
@@ -1,4 +1,5 @@
 python -m pip install --upgrade pip
 pip install git+https://github.com/huggingface/transformers
-pip install qwen-vl-utils accelerate datasets deepspeed peft transformers_stream_generator matplotlib tiktoken optimum auto-gptq
+pip install qwen-vl-utils accelerate datasets deepspeed peft transformers_stream_generator matplotlib tiktoken optimum auto-gptq wandb
+pip install -U "huggingface_hub[cli]"
 pip install flash-attn --no-build-isolation
--- qwen_ft.py
+++ qwen_ft.py
@@ -331,7 +331,7 @@ def train():
         cache_dir=training_args.cache_dir,
         model_max_length=training_args.model_max_length,
         padding_side="right",
-        use_fast=False,
+        use_fast=True,
         trust_remote_code=True,
     )
     # tokenizer.pad_token_id = tokenizer.eod_id
