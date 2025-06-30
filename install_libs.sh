ff4421f6a0991404b795e7f1022acefa54d2ca59
PrAsAnNaRePo <prasannatwenty@gmail.com>
2025-06-30 14:11:17 +0530

    upgrading the packages with apt

--- install_libs.sh
+++ install_libs.sh
@@ -1,3 +1,4 @@
+apt-get update
 python -m pip install --upgrade pip
 pip install git+https://github.com/huggingface/transformers
 pip install qwen-vl-utils accelerate datasets deepspeed peft transformers_stream_generator matplotlib tiktoken optimum auto-gptq wandb
